ed10ef5d40bbfa8e38663b1bc9fe7d9dcb4bd4ef
Álvaro Veiga <alvaromrveiga@gmail.com>
2024-08-01 00:22:33 -0300

    Update (base update)
    [ghstack-poisoned]

--- server/src/controllers/ItensController.ts
+++ server/src/controllers/ItensController.ts
@@ -1,22 +1,22 @@
-import { Request, Response } from 'express';
-import knex from '../database/connection';
+import { Request, Response } from "express";
+import knex from "../database/connection";
 
 class ItensController {
-    async index(request: Request, response: Response) {
-        //await faz esperar carregar tudo, usando await é necessário usar o async na função
-        const itens = await knex('itens').select('*');
+  async index(request: Request, response: Response) {
+    //await faz esperar carregar tudo, usando await é necessário usar o async na função
+    const itens = await knex("itens").select("*");
 
-        //Serialized significa que estou transformando os dados para uma forma mais fácil de acessar por quem recebê-los
-        const serializedItens = itens.map(item => {
-            return {
-                id: item.id,
-                title: item.title,
-                image_url: `/uploads/${item.image}`,
-            }
-        });
+    //Serialized significa que estou transformando os dados para uma forma mais fácil de acessar por quem recebê-los
+    const serializedItens = itens.map((item) => {
+      return {
+        id: item.id,
+        title: item.title,
+        image_url: `/uploads/${item.image}`,
+      };
+    });
 
-        return response.json(serializedItens);
-    }
+    return response.json(serializedItens);
+  }
 }
 
 export default ItensController;
